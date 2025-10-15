958c124fe4e690b4e1f50c0b7ee2a329ce02993a
Sebastian Pino <se.pinob@duocuc.cl>
2025-10-15 15:14:39 -0300

    Conflictos faltantes arreglados

--- app/components/organisms/MainBody/MainBody.tsx
+++ app/components/organisms/MainBody/MainBody.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import TextMenu from "~/components/molecules/MainBody/TextMenu";
-<<<<<<< HEAD
 import { Button, Flex } from "antd";
 
 const MainBody = () =>{
@@ -9,16 +8,6 @@ const MainBody = () =>{
          <TextMenu></TextMenu>
          <Button type="primary" size="large" style={{ width: "fit-content" }}>Mis proyectos</Button>
       </Flex>
-=======
-import { Button } from "antd";
-
-const MainBody = () =>{
-   return(
-      <div>
-         <TextMenu></TextMenu>
-         <Button type="primary">Mis proyectos</Button>
-      </div>
->>>>>>> 72e7ec48402e1eced2cdf5930c71fec33829c06f
       
    )
 }
